1c4ef62bf19ab526295071f7c4006d1edfa4c1a8
Dan Gilbert <ddg@daentech.co.uk>
2021-12-06 17:28:35 +0100

    Add day 4 part 2

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -116,9 +116,92 @@ export const DayClass = {
         Part 2
     */
 
-    part2TestResult: -1,
+    part2TestResult: 1924,
     part2: (input: Game) => {
-        throw "Part 2 not implemented";
+        const boardWidth = input.boards[0][0].length;
+        const boardHeight = input.boards[0].length;
+
+        const markers = Array.from({ length: input.boards.length }, () => Array.from({ length: boardHeight }, () => Array.from({ length: boardWidth }, () => 0)));
+        const winningBoards: number[] = [];
+
+        const winCheck = (bingo: number[][]) => {
+            // check for all values equals to 1 in any horizontal row:
+            let winner;
+            for (var i = 0; i < bingo.length; i++) {
+                winner = true;
+
+                for (var j = 0; j < bingo[i].length; j++) {
+                    if (bingo[i][j] != 1) {
+                        winner = false;
+                        break;
+                    }
+                }
+
+                if (winner) {
+                    return true;
+                }
+            }
+
+            for (var i = 0; i < bingo.length; i++) {
+                winner = true;
+
+                for (var j = 0; j < bingo[i].length; j++) {
+                    if (bingo[j][i] != 1) {
+                        winner = false;
+                        break;
+                    }
+                }
+
+                if (winner) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        const sumBoardUnmatched = (board: number[][], matchers: number[][]): number => {
+            let sum = 0;
+            for (var i = 0; i < board.length; i++) {
+                for (var j = 0; j < board[0].length; j++) {
+                    if (matchers[i][j] == 0) {
+                        sum += board[i][j];
+                    }
+                }
+            }
+            return sum;
+        }
+
+
+        for (var i = 0; i < input.inputs.length; i++) {
+
+            const currentInput = input.inputs[i];
+            for (var j = 0; j < input.boards.length; j++) {
+                // On every board
+                if (winningBoards.includes(j)) {
+                    continue;
+                }
+                const currentBoard = input.boards[j];
+                for (var k = 0; k < boardWidth * boardHeight; k++) {
+                    // Check the current input in every spot on the board
+                    const row = Math.floor(k / boardWidth);
+                    const column = k % boardWidth;
+                    if (currentBoard[row][column] === currentInput) {
+                        markers[j][row][column] = 1;
+                    }
+                }
+                if (i >= Math.min(boardWidth, boardHeight)) {
+                    if (winCheck(markers[j])) {
+
+                        if (winningBoards.length === input.boards.length - 1) {
+                            return sumBoardUnmatched(currentBoard, markers[j]) * currentInput;
+                        }
+
+                        // Remove the board from the equation as it has already won
+                        winningBoards.push(j);
+                    }
+                }
+            }
+        }
     },
 
 }
