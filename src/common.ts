826d6e3b773535a2b40c2f132574d4516e4691f4
Dan Gilbert <ddg@daentech.co.uk>
2021-12-06 17:21:38 +0100

    Add day 4 part 1

--- src/common.ts
+++ src/common.ts
@@ -6,6 +6,7 @@ import { assert } from 'console';
 (async () => {
     const day = process.argv[2];
     const part = process.argv[3];
+    const test = process.argv[4];
 
     const testInput: string[] = fs.readFileSync(`src/${day}/input/test.txt`).toString().split("\n");
     const input: string[] = fs.readFileSync(`src/${day}/input/input.txt`).toString().split("\n");
@@ -14,6 +15,7 @@ import { assert } from 'console';
 
     const runPart1 = part === undefined || part === '1';
     const runPart2 = part === undefined || part === '2';
+    const runTestsOnly = test === "t";
 
     if (runPart1) {
 
@@ -22,7 +24,9 @@ import { assert } from 'console';
 
         const part1TestResult = dayObject.part1(mappedTestInput);
         assert(part1TestResult === dayObject.part1TestResult, `Part 1 (Test input): ${part1TestResult} - should be ${dayObject.part1TestResult}`)
-        console.log(`Part 1 (Real input): ${dayObject.part1(mappedInput)}`);
+        if (!runTestsOnly) {
+            console.log(`Part 1 (Real input): ${dayObject.part1(mappedInput)}`);
+        }
     }
 
     if (runPart2) {
@@ -32,7 +36,10 @@ import { assert } from 'console';
 
         const part2TestResult = dayObject.part2(mappedTestInput);
         assert(part2TestResult === dayObject.part2TestResult, `Part 2 (Test input): ${part2TestResult} - should be ${dayObject.part2TestResult}`)
-        console.log(`Part 2 (Real input): ${dayObject.part2(mappedInput)}`);
+        if (!runTestsOnly) {
+            console.log(`Part 2 (Real input): ${dayObject.part2(mappedInput)}`);
+        }
+
     }
 })().catch((error) => {
     console.log("Error occurred:");
---
+++ src/day4/index.ts
@@ -0,0 +1,124 @@
+import _ from 'lodash';
+
+interface Game {
+    inputs: number[];
+    boards: number[][][];
+}
+
+export const DayClass = {
+
+    mapInput: (input: string[]): Game => {
+
+        const boardsInput = input.slice(2);
+        let currentBoard: number[][] = [];
+        const boards: number[][][] = [];
+
+        _(boardsInput).forEach((row) => {
+            if (row.trim().length == 0) {
+                boards.push([...currentBoard]);
+                currentBoard = [];
+            } else {
+                currentBoard.push(row.trim().split(/\s+/).map((value) => parseInt(value)));
+            }
+        });
+
+        return {
+            inputs: input[0].split(",").map((value) => parseInt(value)),
+            boards,
+        }
+    },
+
+    /*
+        Part 1
+    */
+
+    part1TestResult: 4512,
+    part1: (input: Game) => {
+        const boardWidth = input.boards[0][0].length;
+        const boardHeight = input.boards[0].length;
+
+        const markers = Array.from({ length: input.boards.length }, () => Array.from({ length: boardHeight }, () => Array.from({ length: boardWidth }, () => 0)));
+
+        const winCheck = (bingo: number[][]) => {
+            // check for all values equals to 1 in any horizontal row:
+            let winner;
+            for (var i = 0; i < bingo.length; i++) {
+                winner = true;
+
+                for (var j = 0; j < bingo[i].length; j++) {
+                    if (bingo[i][j] != 1) {
+                        winner = false;
+                        break;
+                    }
+                }
+
+                if (winner) {
+                    return true;
+                }
+            }
+
+            for (var i = 0; i < bingo.length; i++) {
+                winner = true;
+
+                for (var j = 0; j < bingo[i].length; j++) {
+                    if (bingo[j][i] != 1) {
+                        winner = false;
+                        break;
+                    }
+                }
+
+                if (winner) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        const sumBoardUnmatched = (board: number[][], matchers: number[][]): number => {
+            let sum = 0;
+            for (var i = 0; i < board.length; i++) {
+                for (var j = 0; j < board[0].length; j++) {
+                    if (matchers[i][j] == 0) {
+                        sum += board[i][j];
+                    }
+                }
+            }
+            return sum;
+        }
+
+
+        for (var i = 0; i < input.inputs.length; i++) {
+
+            const currentInput = input.inputs[i];
+            for (var j = 0; j < input.boards.length; j++) {
+                // On every board
+                const currentBoard = input.boards[j];
+                for (var k = 0; k < boardWidth * boardHeight; k++) {
+                    // Check the current input in every spot on the board
+                    const row = Math.floor(k / boardWidth);
+                    const column = k % boardWidth;
+                    if (currentBoard[row][column] === currentInput) {
+                        markers[j][row][column] = 1;
+                    }
+                }
+                if (i >= Math.min(boardWidth, boardHeight)) {
+                    if (winCheck(markers[j])) {
+                        // console.log(`Found a winner after ${i} inputs. Current input: ${currentInput}`);
+                        // console.log(markers[j]);
+                        return sumBoardUnmatched(currentBoard, markers[j]) * currentInput;
+                    }
+                }
+            }
+        }
+    },
+
+    /*
+        Part 2
+    */
+
+    part2TestResult: -1,
+    part2: (input: Game) => {
+        throw "Part 2 not implemented";
+    },
+
+}
